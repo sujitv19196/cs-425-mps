9b6f1c3bd0b3084ee6d77dd6d56c5f55466a4d23
sujitv2 <sujitv2@fa22-cs425-8801.cs.illinois.edu>
2022-09-07 15:49:29 -0500

    change params

--- mp1/src/server.cpp
+++ mp1/src/server.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 	char* logfile = argv[1];
 
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET;;
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
